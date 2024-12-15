d4302be9e6d0d7be5f4268653a99bfcc8f144378
Sassi Ben Hammouda <sassihammouda49@gmail.com>
2024-12-15 03:06:54 +0100

    allow all origin cors

--- Backend/public/index.php
+++ Backend/public/index.php
@@ -1,9 +1,7 @@
 <?php
 
-$allowedOrigins = ['http://localhost:8080', 'http://localhost:8100','https://contact-management-ffn7c6igg-sassibhs-projects.vercel.app','*'];
-if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-}
+
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST,PUT,DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
