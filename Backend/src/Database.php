6045261696e1962e9bee5df4e82b0e27067e3b8b
Sassi Ben Hammouda <sassihammouda49@gmail.com>
2024-12-15 04:01:16 +0100

    database connection

--- Backend/src/Database.php
+++ Backend/src/Database.php
@@ -10,13 +10,25 @@ class Database
     public static function getConnection()
     {
         if (!self::$connection) {
-            self::$connection = new PDO(
-                'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8mb4',
-                $_ENV['DB_USER'],
-                $_ENV['DB_PASS']
-            );
+            if (isset($_ENV['CONNECT']) && $_ENV['CONNECT'] === 'true') {
+                // Connect using the environment variables (as usual)
+                self::$connection = new PDO(
+                    'mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ';charset=utf8mb4',
+                    $_ENV['DB_USER'],
+                    $_ENV['DB_PASS']
+                );
+            } else {
+                // Connect to the default database using the hardcoded credentials
+                self::$connection = new PDO(
+                    'mysql:host=mysql-176b02df-contat-manager49-044f.f.aivencloud.com;port=25083;dbname=defaultdb;charset=utf8mb4',
+                    'avnadmin',
+                    'AVNS_OQZ0O1Ux8Ix86ssbiN9'
+                );
+            }
+
             self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
+
         return self::$connection;
     }
 }
